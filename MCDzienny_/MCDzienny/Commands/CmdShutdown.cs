08f6fd35af74a894409c5b5d71c93750ec9ae3cc
p1glynlol <87266022+p1glynlol@users.noreply.github.com>
2023-01-23 12:14:18 +0800

    Update CmdShutdown.cs

--- MCDzienny_/MCDzienny/Commands/CmdShutdown.cs
+++ MCDzienny_/MCDzienny/Commands/CmdShutdown.cs
@@ -6,54 +6,39 @@ using MCDzienny_.Gui;
 
 namespace MCDzienny
 {
-    // Token: 0x020000E1 RID: 225
     internal class CmdShutdown : Command
     {
-        // Token: 0x0400038B RID: 907
         private static readonly Timer shutdownTimer = new Timer();
 
-        // Token: 0x0400038C RID: 908
         private static int lastSeconds = 3;
 
-        // Token: 0x0400038D RID: 909
         private static bool shutdownInProgress;
 
-        // Token: 0x17000342 RID: 834
-        // (get) Token: 0x06000744 RID: 1860 RVA: 0x00024D60 File Offset: 0x00022F60
         public override string name
         {
             get { return "shutdown"; }
         }
-
-        // Token: 0x17000343 RID: 835
-        // (get) Token: 0x06000745 RID: 1861 RVA: 0x00024D68 File Offset: 0x00022F68
+        
         public override string shortcut
         {
             get { return ""; }
         }
 
-        // Token: 0x17000344 RID: 836
-        // (get) Token: 0x06000746 RID: 1862 RVA: 0x00024D70 File Offset: 0x00022F70
         public override string type
         {
             get { return "mod"; }
         }
 
-        // Token: 0x17000345 RID: 837
-        // (get) Token: 0x06000747 RID: 1863 RVA: 0x00024D78 File Offset: 0x00022F78
         public override bool museumUsable
         {
             get { return true; }
         }
 
-        // Token: 0x17000346 RID: 838
-        // (get) Token: 0x06000748 RID: 1864 RVA: 0x00024D7C File Offset: 0x00022F7C
         public override LevelPermission defaultRank
         {
             get { return LevelPermission.Admin; }
         }
 
-        // Token: 0x06000749 RID: 1865 RVA: 0x00024D80 File Offset: 0x00022F80
         public override void Use(Player p, string message)
         {
             if (message != "")
@@ -84,7 +69,6 @@ namespace MCDzienny
             Server.shuttingDown = true;
         }
 
-        // Token: 0x0600074A RID: 1866 RVA: 0x00024E70 File Offset: 0x00023070
         public void RestartTimerCallback1(object sender, ElapsedEventArgs e)
         {
             if (!shutdownInProgress) return;
@@ -95,7 +79,6 @@ namespace MCDzienny
             shutdownTimer.Elapsed += RestartTimerCallback2;
         }
 
-        // Token: 0x0600074B RID: 1867 RVA: 0x00024EE0 File Offset: 0x000230E0
         public void RestartTimerCallback2(object sender, ElapsedEventArgs e)
         {
             if (!shutdownInProgress) return;
@@ -108,8 +91,7 @@ namespace MCDzienny
                 Shutdown();
             }
         }
-
-        // Token: 0x0600074C RID: 1868 RVA: 0x00024F60 File Offset: 0x00023160
+        
         public void Shutdown()
         {
             GuiSettings.All.Save();
@@ -129,7 +111,6 @@ namespace MCDzienny
             Process.GetCurrentProcess().Kill();
         }
 
-        // Token: 0x0600074D RID: 1869 RVA: 0x00024FDC File Offset: 0x000231DC
         public override void Help(Player p)
         {
             Player.SendMessage(p, "/shutdown - shuts down the server within 20 seconds,");
