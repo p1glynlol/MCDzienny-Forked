91c34f12f9dbe8091c18507a43e78778172c4829
p1glynlol <87266022+p1glynlol@users.noreply.github.com>
2023-01-23 12:17:07 +0800

    Update CmdAfk.cs

--- MCDzienny_/MCDzienny/Commands/CmdAfk.cs
+++ MCDzienny_/MCDzienny/Commands/CmdAfk.cs
@@ -2,59 +2,44 @@
 
 namespace MCDzienny
 {
-    // Token: 0x02000251 RID: 593
     internal class CmdAfk : Command
     {
-        // Token: 0x1700062C RID: 1580
-        // (get) Token: 0x06001133 RID: 4403 RVA: 0x0005DC88 File Offset: 0x0005BE88
+
         public override string name
         {
             get { return "afk"; }
         }
-
-        // Token: 0x1700062D RID: 1581
-        // (get) Token: 0x06001134 RID: 4404 RVA: 0x0005DC90 File Offset: 0x0005BE90
+        
         public override string shortcut
         {
             get { return ""; }
         }
 
-        // Token: 0x1700062E RID: 1582
-        // (get) Token: 0x06001135 RID: 4405 RVA: 0x0005DC98 File Offset: 0x0005BE98
         public override string type
         {
             get { return "information"; }
         }
 
-        // Token: 0x1700062F RID: 1583
-        // (get) Token: 0x06001136 RID: 4406 RVA: 0x0005DCA0 File Offset: 0x0005BEA0
         public override bool museumUsable
         {
             get { return true; }
         }
 
-        // Token: 0x17000630 RID: 1584
-        // (get) Token: 0x06001137 RID: 4407 RVA: 0x0005DCA4 File Offset: 0x0005BEA4
         public override LevelPermission defaultRank
         {
             get { return LevelPermission.Guest; }
         }
 
-        // Token: 0x17000631 RID: 1585
-        // (get) Token: 0x06001138 RID: 4408 RVA: 0x0005DCA8 File Offset: 0x0005BEA8
         public override bool ConsoleAccess
         {
             get { return false; }
         }
 
-        // Token: 0x17000632 RID: 1586
-        // (get) Token: 0x06001139 RID: 4409 RVA: 0x0005DCAC File Offset: 0x0005BEAC
         public override string CustomName
         {
             get { return Lang.Command.AfkName; }
         }
-
-        // Token: 0x0600113A RID: 4410 RVA: 0x0005DCB4 File Offset: 0x0005BEB4
+        
         public override void Use(Player p, string message)
         {
             if (!(message != "list"))
@@ -96,7 +81,6 @@ namespace MCDzienny
             Player.IRCSay(string.Format(Lang.Command.AfkMessage1, p.PublicName, message));
         }
 
-        // Token: 0x0600113B RID: 4411 RVA: 0x0005DE50 File Offset: 0x0005C050
         public override void Help(Player p)
         {
             Player.SendMessage(p, Lang.Command.AfkHelp);
